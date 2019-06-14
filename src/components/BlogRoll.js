f03e19b4df18a56a0a3e992d0bd91fc97c8d2789
Luis Camacho <luis.camacho@itcparts.com>
2019-06-14 11:22:25 +0100

    Add some green and some translations

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -49,7 +49,7 @@ class BlogRoll extends React.Component {
                   <br />
                   <br />
                   <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
+                    Continuar a Ler →
                   </Link>
                 </p>
               </article>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -42,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{  maxHeight: '3rem' }} />
+              <img src={logo} alt="Somos Natureza" style={{  maxHeight: '3rem' }} />
             </Link>
             {/* Hamburger menu */}
             <div
