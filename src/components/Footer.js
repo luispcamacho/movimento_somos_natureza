f7ab9bc0177cf2b78ca9f1d38b8589d082d1d821
Luis Camacho <luis.camacho@itcparts.com>
2019-06-23 22:04:47 +0100

    fix instagram

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 import logo from "../img/logo_vertical.svg";
 import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram";
+import instagram from "../img/social/instagram.svg";
 // import twitter from "../img/social/twitter.svg";
 // import vimeo from "../img/social/vimeo.svg";
 
