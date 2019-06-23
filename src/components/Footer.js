c52bea32e311d47c377bcadeca1b313dcff24990
Luis Camacho <luis.camacho@itcparts.com>
2019-06-23 22:03:53 +0100

    fix instagram typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 import logo from "../img/logo_vertical.svg";
 import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
+import instagram from "../img/social/instagram";
 // import twitter from "../img/social/twitter.svg";
 // import vimeo from "../img/social/vimeo.svg";
 
@@ -81,8 +81,7 @@ const Footer = class extends React.Component {
                   />
                 </a> */}
                 <a title="instagram" href="https://www.instagram.com/movimentosomosnatureza/">
-                  <imgcls
-
+                  <img
                     src={instagram}
                     alt="Instagram"
                     style={{ width: "1em", height: "1em" }}
